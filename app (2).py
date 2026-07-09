ddada98b129df609f8c88cc63bab3b204fa451df
Archu403 <archuarchu403@gmail.com>
2026-07-09 15:50:16 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -6,10 +6,10 @@ import seaborn as sns
 st.set_page_config(page_title="EduPro Dashboard", layout="wide")
 
 df = pd.read_csv("EduPro_Final_Dataset.csv")
-st.dataframe(top)
+
 st.download_button(...)
 ...
-st.dataframe(top)
+
 
 # ============================
 # Teacher Rating Distribution
